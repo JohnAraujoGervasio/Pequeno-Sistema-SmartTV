1dd5f90971b777b8b0687101a2ce00f3303c0cf6
john <nayltonjohnj@gmail.com>
2026-03-11 19:39:06 -0300

    Criação das Classes SmartTV e Usuario, e exclusão da classe App.java

---
+++ projeto-sistema-smarttv/src/Usuario.java
@@ -0,0 +1,19 @@
+public class Usuario {
+    public static void main(String[] args) throws Exception {
+        
+        SmartTV smartTV = new SmartTV();
+    
+            System.out.println("A TV está ligada? " + smartTV.ligada);
+            System.out.println("Canal atual: " + smartTV.canal);
+            System.out.println("Volume atual: " + smartTV.volume);
+    
+            smartTV.ligada = true;
+            System.out.println("A TV está ligada? " + smartTV.ligada);
+    
+            smartTV.canal = 10;
+            System.out.println("Canal atual: " + smartTV.canal);
+    
+            smartTV.volume = 35;
+            System.out.println("Volume atual: " + smartTV.volume);
+    }
+}
